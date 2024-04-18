5e44cddf9d19260712857f54752db4a6372066d5
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-04-18 09:40:04 -0300

    Declared building controller route

---
+++ API/BuildingBuddy.API/Controllers/BuildingController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildingBuddy.API.Controllers
+{
+    [Route("api/v1/buildings")]
+    [ApiController]
+    public class BuildingController : ControllerBase
+    {
+        
+        
+        
+    }
+}
