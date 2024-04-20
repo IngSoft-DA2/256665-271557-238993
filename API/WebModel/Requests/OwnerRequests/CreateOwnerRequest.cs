5438b291855f92cd50a44eb171cebe2db7b4e83c
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-04-20 14:49:42 -0300

    [REFACTOR] Removed unnecessary owner creation class

--- API/WebModel/Requests/OwnerRequests/CreateOwnerRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace WebModel.Requests.OwnerRequests;
-
-public class CreateOwnerRequest
-{
-    public string Name { get; set; }
-    public string Lastname { get; set; }
-    public string Email { get; set; }
-}
