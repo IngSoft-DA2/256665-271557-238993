df642a0e10067290b77ca017bc27cb3e466e0107
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2024-04-22 15:09:17 -0300

    [GREEN] CreateAdmin, create with no problem

---
+++ API/Adapter/AdministratorAdapter.cs
@@ -0,0 +1,25 @@
+﻿using IServiceLogic;
+using WebModel.Responses.AdministratorResponses;
+
+namespace Adapter;
+
+public class AdministratorAdapter
+{
+    private readonly IAdministratorService _administratorAdapter;
+    
+    public AdministratorAdapter(IAdministratorService administratorAdapter)
+    {
+        _administratorAdapter = administratorAdapter;
+    }
+    
+    public CreateAdministratorResponse CreateAdministrator(Guid administratorId)
+    {
+        _administratorAdapter.CreateAdministrator(administratorId);
+        CreateAdministratorResponse adapterResponse = new CreateAdministratorResponse()
+        {
+            Id = administratorId
+        };
+        
+        return adapterResponse;
+    }
+}
---
+++ API/Test/Adapters/AdministratorAdapterTest.cs
@@ -0,0 +1,24 @@
+﻿using Adapter;
+using IServiceLogic;
+using Moq;
+using WebModel.Responses.AdministratorResponses;
+
+namespace Test.Adapters;
+
+[TestClass]
+public class AdministratorAdapterTest
+{
+    [TestMethod]
+    public void CreateAdministrator_ShouldCreateAdminsitrator()
+    {
+        Guid administratorId = Guid.NewGuid();
+        
+        Mock<IAdministratorService> administratorService = new Mock<IAdministratorService>(MockBehavior.Strict);
+        administratorService.Setup(service => service.CreateAdministrator(administratorId));
+        AdministratorAdapter administratorAdapter = new AdministratorAdapter(administratorService.Object);
+        
+        CreateAdministratorResponse response = administratorAdapter.CreateAdministrator(administratorId);
+        
+        Assert.AreEqual(administratorId, response.Id);
+    }
+}
