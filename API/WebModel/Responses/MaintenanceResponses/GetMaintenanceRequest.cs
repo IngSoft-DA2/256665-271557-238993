354717c99d6c295f55b1fbb1d200a41d8411554d
sGonzalez06 <113132935+sGonzalez06@users.noreply.github.com>
2024-04-22 15:30:03 -0300

    [REFACTOR] Added more fields in maintenance request response

--- API/WebModel/Responses/MaintenanceResponses/GetMaintenanceRequest.cs
+++ API/WebModel/Responses/MaintenanceResponses/GetMaintenanceRequest.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices.JavaScript;
+
 namespace WebModel.Responses.MaintenanceResponses;
 
 public class GetMaintenanceRequestResponse
@@ -7,6 +9,8 @@ public class GetMaintenanceRequestResponse
     public string? Description { get; set; }
     public Guid FlatId { get; set; }
     public Guid Category { get; set; }
+    public DateTime? OpenedDate { get; set; }
+    public DateTime? ClosedDate { get; set; }
     public Guid? WorkerId { get; set; }
     public StatusEnumMaintenanceResponse RequestStatus { get; set; }
     
