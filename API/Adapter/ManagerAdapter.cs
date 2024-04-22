704e8e461ffec19c52f066b5b484924703bfd2ef
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2024-04-22 14:23:48 -0300

    [GREEN] GetAllManagers

--- API/Adapter/ManagerAdapter.cs
+++ API/Adapter/ManagerAdapter.cs
@@ -7,30 +7,24 @@ namespace Adapter;
 public class ManagerAdapter
 {
     private readonly IManagerService _managerServiceLogic;
-    
+
     public ManagerAdapter(IManagerService managerServiceLogic)
     {
         _managerServiceLogic = managerServiceLogic;
     }
-    
+
     public IEnumerable<GetManagerResponse> GetAllManagers()
     {
-        try
-        {
-            IEnumerable<Manager> serviceResponse = _managerServiceLogic.GetAllManagers();
+        IEnumerable<Manager> serviceResponse = _managerServiceLogic.GetAllManagers();
 
-            IEnumerable<GetManagerResponse> adapterResponse = serviceResponse.Select(manager =>
-                new GetManagerResponse
-                {
-                    Email = manager.Email,
-                    Name = manager.Name,
-                });
+        IEnumerable<GetManagerResponse> adapterResponse = serviceResponse.Select(manager =>
+            new GetManagerResponse
+            {
+                Id = manager.Id,
+                Email = manager.Email,
+                Name = manager.Name,
+            });
 
-            return adapterResponse;
-        }
-        catch (Exception exceptionCaught)
-        {
-            throw new Exception(exceptionCaught.Message);
-        }
+        return adapterResponse;
     }
 }
---
+++ API/Test/Adapters/ManagerAdapterTest.cs
@@ -0,0 +1,46 @@
+﻿using Adapter;
+using Domain;
+using IServiceLogic;
+using Moq;
+using WebModel.Responses.ManagerResponses;
+
+namespace Test.Adapters;
+
+[TestClass]
+
+public class ManagerAdapterTest
+{
+    [TestMethod]
+    public void GetAllManagers_ShouldConvertFromDomainToResponse()
+    {
+        IEnumerable<Manager> domainResponse = new List<Manager>()
+        {
+            new Manager
+            {
+                Id = Guid.NewGuid(),
+                Name = "Michael Kent",
+                Email = "",
+                Password = ""
+            }
+        };
+
+        IEnumerable<GetManagerResponse> expectedAdapterResponse = new List<GetManagerResponse>()
+        {
+            new GetManagerResponse
+            {
+                Id = domainResponse.First().Id,
+                Name = domainResponse.First().Name,
+                Email = domainResponse.First().Email
+            }
+        };
+        
+        Mock<IManagerService> managerService = new Mock<IManagerService>(MockBehavior.Strict);
+        managerService.Setup(service => service.GetAllManagers()).Returns(domainResponse);
+        
+        ManagerAdapter managerAdapter = new ManagerAdapter(managerService.Object);
+        
+        IEnumerable<GetManagerResponse> adapterResponse = managerAdapter.GetAllManagers();
+        
+        Assert.IsTrue(expectedAdapterResponse.SequenceEqual(adapterResponse));
+    }
+}
