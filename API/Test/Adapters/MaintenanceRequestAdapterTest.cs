9f528320e8996149606a9a290e3be9b9f096f0ab
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2024-04-27 02:26:59 -0300

    [GREEN] Update maintenance, throws not foun exception

--- API/Test/Adapters/MaintenanceRequestAdapterTest.cs
+++ API/Test/Adapters/MaintenanceRequestAdapterTest.cs
@@ -204,4 +204,21 @@ public class MaintenanceRequestAdapterTest
         _maintenanceRequestService.Verify(service => service.UpdateMaintenanceRequest(It.IsAny<Guid>(), It.IsAny<MaintenanceRequest>()), Times.Once);
     }
     
+    [TestMethod]
+    public void UpdateMaintenanceRequest_ShouldThrowObjectNotFoundAdapterException()
+    {
+        UpdateMaintenanceRequestStatusRequest updateRequest = new UpdateMaintenanceRequestStatusRequest
+        {
+            RequestStatus = (StatusEnumMaintenanceRequest) StatusEnum.Accepted
+        };
+        
+        _maintenanceRequestService.Setup(service => service.UpdateMaintenanceRequest(It.IsAny<Guid>(), It.IsAny<MaintenanceRequest>()))
+            .Throws(new ObjectNotFoundServiceException());
+        
+        Assert.ThrowsException<ObjectNotFoundAdapterException>(() =>
+            _maintenanceRequestAdapter.UpdateMaintenanceRequest(genericMaintenanceRequest.Id, updateRequest));
+        
+        _maintenanceRequestService.Verify(service => service.UpdateMaintenanceRequest(It.IsAny<Guid>(), It.IsAny<MaintenanceRequest>()), Times.Once);
+    }
+    
 }
