16d041f39e8146345cca2abafe46599371c80819
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2024-04-28 19:33:01 -0300

    [REFACTOR] Initialize approach

--- API/Test/Services/ManagerServiceTest.cs
+++ API/Test/Services/ManagerServiceTest.cs
@@ -10,36 +10,42 @@ namespace Test.Services;
 
 public class ManagerServiceTest
 {
+    
+    private Mock<IManagerRepository> _managerRepository;
+    private ManagerService _managerService;
+    
+    [TestInitialize]
+    public void Initialize()
+    {
+        _managerRepository = new Mock<IManagerRepository>();
+        _managerService = new ManagerService(_managerRepository.Object);
+    }
+    
     [TestMethod]
     public void GetAllManagers_ShouldReturnsAllManagers()
     {
-        var managerRepository = new Mock<IManagerRepository>();
-        var managerService = new ManagerService(managerRepository.Object);
-        var expectedManagers = new List<Manager>
+        IEnumerable<Manager> expectedManagers = new List<Manager>
         {
             new Manager { Id = Guid.NewGuid(), Firstname = "Manager 1" },
             new Manager { Id = Guid.NewGuid(), Firstname = "Manager 2" }
         };
         
-        managerRepository.Setup(x => x.GetAllManagers()).Returns(expectedManagers);
+        _managerRepository.Setup(x => x.GetAllManagers()).Returns(expectedManagers);
         
-        var actualManagers = managerService.GetAllManagers();
+        var actualManagers = _managerService.GetAllManagers();
         
         Assert.AreEqual(expectedManagers, actualManagers);
         
-        managerRepository.VerifyAll();
+        _managerRepository.VerifyAll();
     }
     
     [TestMethod]
-    public void GetAllManagers_ShouldThrowUnknownServiceException_WhenRepositoryThrowsException()
+    public void GetAllManagers_ShouldThrowUnknownServiceException()
     {
-        var managerRepository = new Mock<IManagerRepository>();
-        var managerService = new ManagerService(managerRepository.Object);
+        _managerRepository.Setup(x => x.GetAllManagers()).Throws(new Exception());
         
-        managerRepository.Setup(x => x.GetAllManagers()).Throws(new Exception());
+        Assert.ThrowsException<UnknownServiceException>(() => _managerService.GetAllManagers());
         
-        Assert.ThrowsException<UnknownServiceException>(() => managerService.GetAllManagers());
-        
-        managerRepository.VerifyAll();
+        _managerRepository.VerifyAll();
     }
 }
