d5db388b3047304382950644cf8062b3f7010d4e
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2024-04-30 19:48:09 -0300

    [GREEN] Get report when cat is not set, works good

--- API/Test/Repositories/ReportRepositoryTest.cs
+++ API/Test/Repositories/ReportRepositoryTest.cs
@@ -346,4 +346,62 @@ public class ReportRepositoryTest
 
         Assert.IsTrue(expectedMaintenanceRequests.SequenceEqual(maintenanceRequestsResponse));
     }
+    
+    [TestMethod]
+    public void GetMaintenanceReportByCategory_ReturnsMaintenanceRequestsWhenNoCategoryComesFromQuery()
+    {
+        MaintenanceRequest maintenanceRequestInDb2 = new MaintenanceRequest
+        {
+            Id = Guid.NewGuid(),
+            Manager = new Manager()
+            {
+                Id = Guid.NewGuid(),
+                Firstname = "FirstName",
+                Email = "Email",
+                Password = "Password",
+                Buildings = new List<Building>()
+            },
+            RequestHandler = new RequestHandler()
+            {
+                Id = Guid.NewGuid(),
+                Firstname = "FirstName",
+                Email = "Email",
+                Password = "Password",
+                LastName = "LastName"
+            },
+            Category = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = "Category"
+            },
+            Description = "Description",
+            OpenedDate = DateTime.Now,
+            ClosedDate = DateTime.Now,
+            RequestStatus = RequestStatusEnum.Open,
+            Flat = new Flat
+            {
+                Id = Guid.NewGuid(),
+                BuildingId = Guid.NewGuid(),
+                OwnerAssigned = new Owner()
+                {
+                    Id = Guid.NewGuid(),
+                    Firstname = "FirstName",
+                    Email = "Email",
+                    Flats = new List<Flat>(),
+                    Lastname = "Lastname"
+                }
+            }
+        };
+
+        IEnumerable<MaintenanceRequest> expectedMaintenanceRequests =
+            new List<MaintenanceRequest> { maintenanceRequestInDb2 };
+
+        _dbContext.Set<MaintenanceRequest>().Add(maintenanceRequestInDb2);
+        _dbContext.SaveChanges();
+
+        IEnumerable<MaintenanceRequest> maintenanceRequestsResponse =
+            _reportRepository.GetMaintenanceReportByCategory(maintenanceRequestInDb2.Flat.BuildingId, Guid.Empty);
+
+        Assert.IsTrue(expectedMaintenanceRequests.SequenceEqual(maintenanceRequestsResponse));
+    }
 }
