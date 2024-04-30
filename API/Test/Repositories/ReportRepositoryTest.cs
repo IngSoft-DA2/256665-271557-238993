79e392fbbedd132221801e2ea4c7f53e00be2454
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2024-04-30 19:41:25 -0300

    [GREEN] Get report by req handler when no req handler, works

--- API/Test/Repositories/ReportRepositoryTest.cs
+++ API/Test/Repositories/ReportRepositoryTest.cs
@@ -216,6 +216,64 @@ public class ReportRepositoryTest
         Assert.IsTrue(expectedMaintenanceRequests.SequenceEqual(maintenanceRequestsResponse));
     }
     
+    [TestMethod]
+    public void GetMaintenanceReportByRequestHandler_ReturnsMaintenanceRequestsWhenNoRequestHandlerComesFromQuery()
+    {
+        MaintenanceRequest maintenanceRequestInDb2 = new MaintenanceRequest
+        {
+            Id = Guid.NewGuid(),
+            Manager = new Manager()
+            {
+                Id = Guid.NewGuid(),
+                Firstname = "FirstName",
+                Email = "Email",
+                Password = "Password",
+                Buildings = new List<Building>()
+            },
+            RequestHandler = new RequestHandler()
+            {
+                Id = Guid.NewGuid(),
+                Firstname = "FirstName",
+                Email = "Email",
+                Password = "Password",
+                LastName = "LastName"
+            },
+            Category = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = "Category"
+            },
+            Description = "Description",
+            OpenedDate = DateTime.Now,
+            ClosedDate = DateTime.Now,
+            RequestStatus = RequestStatusEnum.Open,
+            Flat = new Flat
+            {
+                Id = Guid.NewGuid(),
+                BuildingId = Guid.NewGuid(),
+                OwnerAssigned = new Owner()
+                {
+                    Id = Guid.NewGuid(),
+                    Firstname = "FirstName",
+                    Email = "Email",
+                    Flats = new List<Flat>(),
+                    Lastname = "Lastname"
+                }
+            }
+        };
+
+        IEnumerable<MaintenanceRequest> expectedMaintenanceRequests =
+            new List<MaintenanceRequest> { maintenanceRequestInDb2 };
+
+        _dbContext.Set<MaintenanceRequest>().Add(maintenanceRequestInDb2);
+        _dbContext.SaveChanges();
+
+        IEnumerable<MaintenanceRequest> maintenanceRequestsResponse =
+            _reportRepository.GetMaintenanceReportByRequestHandler(Guid.Empty, maintenanceRequestInDb2.Flat.BuildingId, maintenanceRequestInDb2.ManagerId);
+
+        Assert.IsTrue(expectedMaintenanceRequests.SequenceEqual(maintenanceRequestsResponse));
+    }
+    
 
     
 }
