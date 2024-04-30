c131513910eabc348385a325e0db3cafbc5adc2c
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2024-04-30 19:15:31 -0300

    [GREEN] Maintenance report, works finde

--- API/Test/Repositories/ReportRepositoryTest.cs
+++ API/Test/Repositories/ReportRepositoryTest.cs
@@ -7,7 +7,6 @@ using Microsoft.EntityFrameworkCore;
 namespace Test.Repositories;
 
 [TestClass]
-
 public class ReportRepositoryTest
 {
     private DbContext _dbContext;
@@ -20,14 +19,14 @@ public class ReportRepositoryTest
         _dbContext.Set<MaintenanceRequest>();
         _reportRepository = new ReportRepository(_dbContext);
     }
-    
+
     private DbContext CreateDbContext(string dbName)
     {
         var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(dbName).Options;
         return new ApplicationDbContext(options);
     }
-    
-    
+
+
     [TestMethod]
     public void GetMaintenanceReportByBuilding_ReturnsMaintenanceRequestsWhenBuildingFromQueryIsSetted()
     {
@@ -41,7 +40,6 @@ public class ReportRepositoryTest
                 Email = "Email",
                 Password = "Password",
                 Buildings = new List<Building>()
-                
             },
             RequestHandler = new RequestHandler()
             {
@@ -74,15 +72,75 @@ public class ReportRepositoryTest
                 }
             }
         };
-        
-        IEnumerable<MaintenanceRequest> expectedMaintenanceRequests = new List<MaintenanceRequest> {maintenanceRequestInDb2};
-    
+
+        IEnumerable<MaintenanceRequest> expectedMaintenanceRequests =
+            new List<MaintenanceRequest> { maintenanceRequestInDb2 };
+
         _dbContext.Set<MaintenanceRequest>().Add(maintenanceRequestInDb2);
         _dbContext.SaveChanges();
-        
-        IEnumerable<MaintenanceRequest> maintenanceRequestsResponse = _reportRepository.GetMaintenanceReportByBuilding(maintenanceRequestInDb2.ManagerId, maintenanceRequestInDb2.Flat.BuildingId);
-        
+
+        IEnumerable<MaintenanceRequest> maintenanceRequestsResponse =
+            _reportRepository.GetMaintenanceReportByBuilding(maintenanceRequestInDb2.ManagerId,
+                maintenanceRequestInDb2.Flat.BuildingId);
+
         Assert.IsTrue(expectedMaintenanceRequests.SequenceEqual(maintenanceRequestsResponse));
     }
-    
+
+    [TestMethod]
+    public void GetMaintenanceReportByBuilding_ReturnsMaintenanceRequestsWhenNoBuildingComesFromQuery()
+    {
+        MaintenanceRequest maintenanceRequestInDb2 = new MaintenanceRequest
+        {
+            Id = Guid.NewGuid(),
+            Manager = new Manager()
+            {
+                Id = Guid.NewGuid(),
+                Firstname = "FirstName",
+                Email = "Email",
+                Password = "Password",
+                Buildings = new List<Building>()
+            },
+            RequestHandler = new RequestHandler()
+            {
+                Id = Guid.NewGuid(),
+                Firstname = "FirstName",
+                Email = "Email",
+                Password = "Password",
+                LastName = "LastName"
+            },
+            Category = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = "Category"
+            },
+            Description = "Description",
+            OpenedDate = DateTime.Now,
+            ClosedDate = DateTime.Now,
+            RequestStatus = RequestStatusEnum.Open,
+            Flat = new Flat
+            {
+                Id = Guid.NewGuid(),
+                BuildingId = Guid.NewGuid(),
+                OwnerAssigned = new Owner()
+                {
+                    Id = Guid.NewGuid(),
+                    Firstname = "FirstName",
+                    Email = "Email",
+                    Flats = new List<Flat>(),
+                    Lastname = "Lastname"
+                }
+            }
+        };
+
+        IEnumerable<MaintenanceRequest> expectedMaintenanceRequests =
+            new List<MaintenanceRequest> { maintenanceRequestInDb2 };
+
+        _dbContext.Set<MaintenanceRequest>().Add(maintenanceRequestInDb2);
+        _dbContext.SaveChanges();
+
+        IEnumerable<MaintenanceRequest> maintenanceRequestsResponse =
+            _reportRepository.GetMaintenanceReportByBuilding(maintenanceRequestInDb2.ManagerId, Guid.Empty);
+
+        Assert.IsTrue(expectedMaintenanceRequests.SequenceEqual(maintenanceRequestsResponse));
+    }
 }
