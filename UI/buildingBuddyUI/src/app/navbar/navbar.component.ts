62e665d33a00e82c882e8657b93cecf13a45ff05
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-06-02 17:37:48 -0300

    Admin navbar functionalities now appears if role is admin

--- UI/buildingBuddyUI/src/app/navbar/navbar.component.ts
+++ UI/buildingBuddyUI/src/app/navbar/navbar.component.ts
@@ -9,11 +9,17 @@ import { Route, Router } from '@angular/router';
 export class NavbarComponent 
 {
 
-  constructor(private router : Router){}
+  userRole : string
+
+  constructor(private router : Router)
+  {
+    // We need to get the user role. Now I hardcoded it, so it can be show up. But is wrong.
+    this.userRole = "Admin";
+  }
 
   logout() : void 
   {
-    // We need to logout the user when the button is clicked.
+    // We need to logout the user when the button is clicked. Need to call service function here.
 
     this.router.navigateByUrl("/")
   }
