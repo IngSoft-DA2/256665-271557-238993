35171b81f4322b13b6facc8c702481a47f5ccf1e
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-06-04 09:46:34 -0300

    Created manager list and introducing delete aspects

--- UI/buildingBuddyUI/src/app/app.module.ts
+++ UI/buildingBuddyUI/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ManagerCreateComponent } from './features/manager/features/manager-crea
 import { ConstructionCompanyAdminCreateComponent } from './features/constructionCompanyAdmin/construction-company-admin-create/construction-company-admin-create.component';
 import { LandingPageComponent } from './features/landingPage/landing-page/landing-page.component';
 import { AdminCreateComponent } from './features/administrator/admin-create/admin-create.component';
+import { ManagerListComponent } from './features/manager/features/manager-list/manager-list.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { AdminCreateComponent } from './features/administrator/admin-create/admi
     ManagerCreateComponent,
     ConstructionCompanyAdminCreateComponent,
     LandingPageComponent,
-    AdminCreateComponent
+    AdminCreateComponent,
+    ManagerListComponent
   ],
   imports: [
     BrowserModule,
---
+++ UI/buildingBuddyUI/src/app/features/manager/features/manager-list/manager-list.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { ManagerService } from '../../services/manager.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-manager-list',
+  templateUrl: './manager-list.component.html',
+  styleUrl: './manager-list.component.css'
+})
+
+
+
+export class ManagerListComponent 
+{
+  managers : Manager[] = [];
+
+  constructor(private managerService : ManagerService, private router : Router){}
+
+
+  getAllManagers() : void 
+  {
+    this.managerService.getAllManagers()
+    .subscribe({
+      next : (Response) => {
+        this.managers = Response;
+      },
+      error : (errorMessage) => {
+        alert(errorMessage.error);
+      }
+    })
+  }
+}
