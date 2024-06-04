468b737394dbae8ae83016793390c6e0b8bd94c7
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-06-04 12:39:15 -0300

    Construction company admin can be created without invitation

--- UI/buildingBuddyUI/src/app/features/constructionCompanyAdmin/construction-company-admin-create-by-invitation/construction-company-admin-create-by-invitation.component.ts
+++ UI/buildingBuddyUI/src/app/features/constructionCompanyAdmin/construction-company-admin-create-by-invitation/construction-company-admin-create-by-invitation.component.ts
@@ -4,16 +4,22 @@ import { Invitation } from '../../invitation/interfaces/invitation';
 import { ActivatedRoute, Router } from '@angular/router';
 import { constructionCompanyAdminCreateRequest } from '../interfaces/construction-company-admin-create-request';
 import { ConstructionCompanyAdminService } from '../services/construction-company-admin.service';
+import { SystemUserRoleEnum } from '../../invitation/interfaces/enums/system-user-role-enum';
+import { StatusEnum } from '../../invitation/interfaces/enums/status-enum';
 
 @Component({
-  selector: 'app-construction-company-admin-create-by-invitation', 
-  templateUrl: './construction-company-admin-create-by-invitation.component.html', 
+  selector: 'app-construction-company-admin-create-by-invitation',
+  templateUrl: './construction-company-admin-create-by-invitation.component.html',
   styleUrls: ['./construction-company-admin-create-by-invitation.component.css']
 })
 export class ConstructionCompanyAdminCreateByInvitationComponent {
 
   invitationOfUser?: Invitation
+  hasInvitation : boolean = false;
+  hasValidRole : boolean = true;
+  
   invitationId: string = '';
+  userRole?: SystemUserRoleEnum; //To do: We need to implement how to get the user role.
 
   constructionCompanyAdminToCreate: constructionCompanyAdminCreateRequest =
     {
@@ -24,39 +30,58 @@ export class ConstructionCompanyAdminCreateByInvitationComponent {
       invitationId: undefined
     };
 
-  constructor(private invitationService: InvitationService,
-    private constructionCompanyAdminService: ConstructionCompanyAdminService, private router: Router, private route: ActivatedRoute) {
+  constructor(
+    private invitationService: InvitationService,
+    private constructionCompanyAdminService: ConstructionCompanyAdminService,
+    private router: Router, private route: ActivatedRoute
+  ) {
+
     this.route.queryParams.subscribe({
       next: (queryParams) => {
-        this.invitationId = queryParams['idOfInvitationAccepted']
-      }
+        this.invitationId = queryParams['idOfInvitationAccepted']      }
     });
 
-    this.invitationService.getInvitationById(this.invitationId)
-      .subscribe({
-        next: (Response) => {
-          this.invitationOfUser = Response;
+    if (this.invitationId !== undefined) {
+      this.invitationService.getInvitationById(this.invitationId)
+        .subscribe({
+          next: (Response) => {
+            this.invitationOfUser = Response;
+            if (this.invitationOfUser.status === StatusEnum.Pending) {
+              this.constructionCompanyAdminToCreate =
+              {
+                firstname: this.invitationOfUser.firstname,
+                lastname: this.invitationOfUser.lastname,
+                email: this.invitationOfUser.email,
+                password: '',
+                invitationId: this.invitationOfUser.id
+              };
+              this.hasInvitation = true;
+            }
+            else{
+              alert("Invitation was found, but it status needs to be pending. Redirecting...");
+              this.router.navigateByUrl('/');
+            }
 
-          this.constructionCompanyAdminToCreate =
-          {
-            firstname: this.invitationOfUser.firstname,
-            lastname: this.invitationOfUser.lastname,
-            email: this.invitationOfUser.email,
-            password: '',
-            invitationId: this.invitationOfUser.id
-          };
+          },
+          error: () => {
+            alert("Invitation was not found, redirecting...");
+            this.router.navigateByUrl('/');
+          }
+        })
+    }
+    else if (this.userRole === SystemUserRoleEnum.ConstructionCompanyAdmin) {
+      this.hasValidRole = true;
+      
+    }
+    else{
+      alert("You do not have the necessary role to enter here, redirecting");
+      this.router.navigateByUrl('/');
+    }
 
-        },
-        error: (errorMessage) => {
-          alert("Invitation was not found, redirecting...");
-          this.router.navigateByUrl('/');
-        }
-      })
   }
 
   createConstructionCompanyAdmin(): void {
-    if (this.invitationOfUser) {
-      alert(this.constructionCompanyAdminToCreate.email)
+    if (this.invitationOfUser || this.userRole == SystemUserRoleEnum.ConstructionCompanyAdmin) {
       this.constructionCompanyAdminService.createConstructionCompanyAdmin(this.constructionCompanyAdminToCreate)
         .subscribe({
           next: () => {
