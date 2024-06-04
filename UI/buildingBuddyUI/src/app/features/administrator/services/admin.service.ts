298d3cbb1fa82d315273214ff7bfa77edb05fc75
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-06-03 22:27:05 -0300

    Created admin with success

---
+++ UI/buildingBuddyUI/src/app/features/administrator/services/admin.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AdminCreateRequest } from '../interfaces/admin-create-request';
+import { AdminCreateResponse } from '../interfaces/admin-create-response';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor(private http: HttpClient) { }
+
+  createAdministrator(adminToCreate: AdminCreateRequest)
+  {
+    return this.http.post<AdminCreateResponse>(`${environment.apiBaseUrl}/api/v2/administrators`,adminToCreate)
+  }
+
+
+
+
+}
