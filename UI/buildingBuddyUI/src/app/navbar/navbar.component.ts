7e8a4cea783f355364df532bd85b328cfcfb6631
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-06-06 09:29:49 -0300

    Adapted user role into undefined

--- UI/buildingBuddyUI/src/app/navbar/navbar.component.ts
+++ UI/buildingBuddyUI/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Route, Router } from '@angular/router';
 
 @Component({
@@ -6,15 +6,20 @@ import { Route, Router } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent 
+export class NavbarComponent implements OnInit
 {
 
-  userRole : string | undefined
+  userRole : string | undefined = undefined
 
   constructor(private router : Router)
   {
     // We need to get the user role. Now I hardcoded it, so it can be show up. But is wrong.
-    this.userRole = "Admin";
+    this.userRole = undefined;
+
+  }
+  ngOnInit(): void
+  {
+    this.userRole = undefined;
   }
 
   logout() : void 
