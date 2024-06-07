46f380252393d4a055041c68b5a1d109e8d78039
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-06-06 23:10:19 -0300

    Created building interface

--- UI/buildingBuddyUI/src/app/features/building/interfaces/building.ts
+++ UI/buildingBuddyUI/src/app/features/building/interfaces/building.ts
@@ -1,4 +1,16 @@
+import { ConstructionCompany } from "../../constructionCompany/interfaces/construction-company";
+import { Manager } from "../../manager/interfaces/manager";
+
 export interface Building 
 {
-    
+    id : string,
+    managerId : string,
+    manager : Manager,
+    name : string,
+    address : string,
+    location : Location,
+    constructionCompanyId : string,
+    constructionCompany : ConstructionCompany,
+    commonExpenses : number,
+    flats : Flat[]
 }
