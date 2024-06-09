753737d6c9742ebb3f8afec0ef055fc3e1e63d19
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2024-06-09 17:51:14 -0300

    [REFACTOR/FIX] Re adapted update owner by id logic

--- UI/buildingBuddyUI/src/app/features/owner/owner-update/owner-update.component.ts
+++ UI/buildingBuddyUI/src/app/features/owner/owner-update/owner-update.component.ts
@@ -9,68 +9,75 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './owner-update.component.html',
   styleUrl: './owner-update.component.css'
 })
-export class OwnerUpdateComponent 
-{
-  ownerId? : string;
+export class OwnerUpdateComponent {
+  ownerId?: string;
+  buildingId?: string;
 
-  ownerToUpdate : OwnerUpdateRequest ={
-    firstname : '',
-    lastname : '',
-    email : ''
+  ownerToUpdate: OwnerUpdateRequest = {
+    firstname: '',
+    lastname: '',
+    email: ''
   };
 
-  constructor(private ownerService : OwnerService, private router : Router, private route : ActivatedRoute)
-  {
+  constructor(private ownerService: OwnerService, private router: Router, private route: ActivatedRoute) {
+
+    this.obtainBuildinIdFromQuery();
+
     this.obtaindOwnerIdFromRoute();
-    if(this.ownerId)
-    {
+    if (this.ownerId) {
       this.ownerService.getOwnerById(this.ownerId)
-      .subscribe({
-        next: (Response) => {
-          this.ownerToUpdate = Response;
-        },
-        error: (errorMessage) => {
-          alert(errorMessage.error);
-          this.router.navigateByUrl('/home') //Same question as owner list
-        }
-      });
+        .subscribe({
+          next: (Response) => {
+            this.ownerToUpdate = Response;
+          },
+          error: (errorMessage) => {
+            alert(errorMessage.error);
+            this.router.navigateByUrl(`/buildings/${this.buildingId}/owners`)
+          }
+        });
     }
-    else{
+    else {
       alert("Owner id was not found, redirecting");
-      this.router.navigateByUrl('/home') //Same question as owner list
+      this.router.navigateByUrl(`/buildings/${this.buildingId}/owners`)
     }
-    
+
   }
-
-
-  updateOwner() : void {
-    if(this.ownerId)
-    {
+  updateOwner(): void {
+    if (this.ownerId) {
       this.ownerService.updateOwner(this.ownerId, this.ownerToUpdate)
-      .subscribe({
-        next: () => {
-          alert("Owner was updated successfully");
-        }
-      })
+        .subscribe({
+          next: () => {
+            alert("Owner was updated successfully");
+            this.router.navigateByUrl(`/buildings/${this.buildingId}/owners`)
+          }
+        })
     }
-    else{
+    else {
       alert("Owner id was not found, redirecting");
-      this.router.navigateByUrl('/home') //Same question as owner list
+      this.router.navigateByUrl(`/buildings/${this.buildingId}/owners`)
     }
-    
+
   }
 
-  private obtaindOwnerIdFromRoute()
-  {
+
+  private obtainBuildinIdFromQuery() {
+
+    this.route.queryParams.subscribe(params => {
+      this.buildingId = params['buildingId'];
+    });
+  }
+
+  private obtaindOwnerIdFromRoute() {
     this.route.paramMap.subscribe({
-      next:(params) => {
+      next: (params) => {
         this.ownerId = params.get('ownerId') ?? undefined;
-        if(this.ownerId === undefined){
+        if (this.ownerId === undefined) {
           alert("Owner was not found, redirecting");
           this.router.navigateByUrl('/home') //Same question as owner list
         }
       }
     })
   }
+
 
 }
