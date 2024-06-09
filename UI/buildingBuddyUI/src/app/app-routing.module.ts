7c2605b6bfef0504d33557c2eec483d6eddca87a
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2024-06-09 01:59:08 -0300

    Deleted imports for merge

--- UI/buildingBuddyUI/src/app/app-routing.module.ts
+++ UI/buildingBuddyUI/src/app/app-routing.module.ts
@@ -5,16 +5,9 @@ import { InvitationListComponent } from './features/invitation/invitation-list/i
 import { InvitationUpdateComponent } from './features/invitation/invitation-update/invitation-update.component';
 import { InvitationCreateComponent } from './features/invitation/invitation-create/invitation-create.component';
 import { InvitationListByEmailComponent } from './features/invitation/invitation-list-by-email/invitation-list-by-email.component';
-import { ManagerCreateComponent } from './features/manager/features/manager-create/manager-create.component';
 import { LandingPageComponent } from './features/landingPage/landing-page/landing-page.component';
 import { AdminCreateComponent } from './features/administrator/admin-create/admin-create.component';
 import { ReportsListComponent } from './features/Reports/reports-list/reports-list.component';
-import { ConstructionCompanyAdminCreateByInvitationComponent } from './features/constructionCompanyAdmin/construction-company-admin-create-by-invitation/construction-company-admin-create-by-invitation.component';
-import { ManagerListComponent } from './features/manager/features/manager-list/manager-list.component';
-import { ConstructionCompanyCreateComponent } from './features/constructionCompany/construction-company-create/construction-company-create.component';
-import { ConstructionCompanyListComponent } from './features/constructionCompany/construction-company-list/construction-company-list.component';
-import { BuildingListComponent } from './features/Building/building-list/building-list.component';
-import { ConstructionCompanyUpdateComponent } from './features/constructionCompany/construction-company-update/construction-company-update.component';
 import { CategoryCreateComponent } from './features/category/category-create/category-create.component';
 import { ReportMaintenanceReqByReqHandlerComponent } from './features/Reports/report-maintenance-req-by-req-handler/report-maintenance-req-by-req-handler.component';
 import { ReportMaintenanceReqByCategoryComponent } from './features/Reports/report-maintenance-req-by-category/report-maintenance-req-by-category.component';
@@ -45,14 +38,6 @@ const routes: Routes =
   {
     path: 'invitations/guests/list',
     component : InvitationListByEmailComponent
-  },
-  {
-    path: 'managers/create',
-    component : ManagerCreateComponent
-  },
-  {
-    path: 'constructionCompanyAdmin/create',
-    component : ConstructionCompanyAdminCreateByInvitationComponent
   },
   {
     path: '',
@@ -93,26 +78,6 @@ const routes: Routes =
   {
     path: 'reports/requests-by-flat',
     component: ReportMaintenanceRequestsByFlatComponent
-  },
-  {
-    path: 'managers/list',
-    component : ManagerListComponent
-  },
-  {
-    path : 'construction-companies/create',
-    component : ConstructionCompanyCreateComponent
-  },
-  {
-    path: 'construction-companies/list',
-    component : ConstructionCompanyListComponent
-  },
-  {
-    path: 'construction-companies/:id/update',
-    component : ConstructionCompanyUpdateComponent
-  },
-  {
-    path : 'buildings/list',
-    component : BuildingListComponent
   },
   {
     path: 'categories/create',
