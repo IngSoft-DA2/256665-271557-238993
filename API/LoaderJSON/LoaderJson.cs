09b63739de539e5d6578de825ad6f447f6776ab7
sGonzalez06 <113132935+sGonzalez06@users.noreply.github.com>
2024-06-10 08:16:32 -0300

    [GREEN] Added building Importers

---
+++ API/LoaderJSON/LoaderJson.cs
@@ -0,0 +1,76 @@
+﻿using System.Text.Json;
+using Domain;
+using ILoaders;
+
+namespace LoaderJSON;
+
+public class LoaderJson : ILoader
+{
+    public string LoaderName()
+    {
+        return "BuildingLoaderJSON";
+    }
+
+    public List<Building> LoadAllBuildings(string filePath)
+    {
+        List<Building> buildings = new List<Building>();
+
+        try
+        {
+            string json = File.ReadAllText(filePath);
+            var serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                AllowTrailingCommas = true
+            };
+
+            BuildingWrapperJson buildingWrapper =
+                JsonSerializer.Deserialize<BuildingWrapperJson>(json, serializerOptions);
+
+            foreach (BuildingJSON building in buildingWrapper.Edificios)
+            {
+                Building newBuilding = new Building()
+                {
+                    Name = building.Nombre,
+                    Address =
+                        $"{building.Direccion.CallePrincipal}, {building.Direccion.NumeroPuerta}, {building.Direccion.CalleSecundaria}",
+                    Manager = new Manager()
+                    {
+                        Email = building.Encargado
+                    },
+                    Location = new Location()
+                    {
+                        Latitude = building.Gps.Latitud,
+                        Longitude = building.Gps.Longitud
+                    },
+                    CommonExpenses = building.GastosComunes
+                };
+
+                foreach (FlatJson flat in building.Departamentos)
+                {
+                    newBuilding.Flats.Append(new Flat()
+                    {
+                        Floor = flat.Piso,
+                        RoomNumber = flat.NumeroPuerta.ToString(),
+                        TotalRooms = flat.Habitaciones,
+                        TotalBaths = flat.Banos,
+                        HasTerrace = flat.ConTerraza,
+                        OwnerAssigned = new Owner()
+                        {
+                            Email = flat.PropietarioEmail,
+                        }
+                    });
+
+                }
+
+                buildings.Add(newBuilding);
+            }
+        }
+        catch (Exception exceptionCaught)
+        {
+            throw new Exception("Error loading buildings from JSON");
+        }
+
+        return buildings;
+    }
+}
---
+++ API/LoaderXML/LoaderXml.cs
@@ -0,0 +1,72 @@
+﻿using System.Xml.Serialization;
+using Domain;
+using ILoaders;
+
+namespace LoaderXML;
+
+public class LoaderXML : ILoader
+{
+    public string LoaderName()
+    {
+        return "BuildingLoaderXML";
+    }
+
+    public List<Building> LoadAllBuildings(string filePath)
+    {
+        List<Building> buildings = new List<Building>();
+
+        try
+        {
+
+            XmlSerializer serializer = new XmlSerializer(typeof(BuildingWrapperXML));
+
+            BuildingWrapperXML buildingWrapper =
+                serializer.Deserialize(new FileStream(filePath, FileMode.Open)) as BuildingWrapperXML;
+
+            foreach (BuildingXML building in buildingWrapper.Edificios)
+            {
+                Building newBuilding = new Building()
+                {
+                    Name = building.Nombre,
+                    Address =
+                        $"{building.Direccion.CallePrincipal}, {building.Direccion.NumeroPuerta}, {building.Direccion.CalleSecundaria}",
+                    Manager = new Manager()
+                    {
+                        Email = building.Encargado
+                    },
+                    Location = new Location()
+                    {
+                        Latitude = building.Gps.Latitud,
+                        Longitude = building.Gps.Longitud
+                    },
+                    CommonExpenses = building.GastosComunes
+                };
+
+                foreach (FlatXML flat in building.Departamentos)
+                {
+                    newBuilding.Flats.Append(new Flat()
+                    {
+                        Floor = flat.Piso,
+                        RoomNumber = flat.NumeroPuerta.ToString(),
+                        TotalRooms = flat.Habitaciones,
+                        TotalBaths = flat.Banos,
+                        HasTerrace = flat.ConTerraza,
+                        OwnerAssigned = new Owner()
+                        {
+                            Email = flat.PropietarioEmail
+                        }
+                    });
+                }
+
+                buildings.Append(newBuilding);
+
+            }
+        }
+        catch (Exception exceptionCaught)
+        {
+            throw new Exception(exceptionCaught.Message);
+        }
+
+        return buildings;
+        }
+}
